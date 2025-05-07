052a35fa3f72e264eaef2e267c551ba5c7a30496
Elias_Office <elias.schill@student.kit.edu>
2025-05-07 11:47:57 +0200

    Relais Test skript in TransferStage ordner kopiert

--- 2_TransferStage/2_Python_Scripts/Test.py
+++ 2_TransferStage/2_Python_Scripts/Test.py
@@ -1,15 +1,9 @@
-import asyncio
-from bleak import BleakClient
+import rly02 as smartPlug
+import time
+smartPlug.turn_relay_1_on()
+smartPlug.turn_relay_2_on()
 
-async def run(address, loop):
-  async with BleakClient(address, loop=loop) as client:
-     await client.connect()
-     services = await client.get_services()
-     for service in services:
-         characteristics = await client.get_characteristics(service.handle)
-         for characteristic in characteristics:
-             await client.read_characteristic(characteristic.handle)
-             await client.write_characteristic(characteristic.handle, b'Hello World')
+time.sleep(2)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(run('CA:D4:DB:59:29:A0', loop))
+smartPlug.turn_relay_1_off()
+smartPlug.turn_relay_2_off()
--- 3_Filtration/2_Python_Scripts/test.py
+++ 3_Filtration/2_Python_Scripts/test.py
@@ -1,11 +1,10 @@
-"""
-import propar
-instrument = propar.instrument("COM5")
-print(instrument.wink(time=3))
-print(instrument.id)
-"""
 
-from Elveflow64 import *
+import rly02 as smartPlug
+import time
+smartPlug.turn_relay_1_on()
+smartPlug.turn_relay_2_on()
 
-Instr_ID = c_int32()
+time.sleep(2)
 
+smartPlug.turn_relay_1_off()
+smartPlug.turn_relay_2_off()
